ca1d5fc81e30dba066a55781311eeaec13f45917
Abel Haddis (Student) <ahaddis@sundevils.asu.edu>
2022-07-15 16:29:05 -0700

    Update etch-a-sketch.js

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,10 @@
+const page = document.querySelector(".page_container");
 const paint_brush = document.querySelector(".paint_brush");
 const canvas = document.querySelector(".canvas");
 const slider = document.querySelector(".slider");
 
 paint_brush.addEventListener("click", function(){
-    canvas.setAttribute("style", "cursor: url('./Images/cursor_brush.png'), auto;");
+    page.setAttribute("style", "cursor: url('./Images/cursor_brush.png'), auto;");
 });
 
 
