d70179e4a3aaa6dbbf743ab52cc0f78caf4e017e
Abel Haddis (Student) <ahaddis@sundevils.asu.edu>
2022-07-15 16:46:16 -0700

    Working on adding squares to canvas

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,6 +3,16 @@ const paint_brush = document.querySelector(".paint_brush");
 const canvas = document.querySelector(".canvas");
 const slider = document.querySelector(".slider");
 
+let canvas_size = document.querySelector(".slider").value;
+let square_size = 500/canvas_size;
+const square = document.createElement("div");
+square.setAttribute("style", `color: red; height: ${square_size}; width: ${square_size};`);
+
+for(let i=0; i<canvas_size; i++)
+{
+    canvas.appendChild(square);
+}
+
 paint_brush.addEventListener("click", function(){
     page.setAttribute("style", "cursor: url('./Images/cursor_brush.png'), auto;");
 });
