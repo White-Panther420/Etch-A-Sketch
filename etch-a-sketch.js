339cf769849079ac165fd778acc1ac49a4c76041
Abel Haddis (Student) <ahaddis@sundevils.asu.edu>
2022-07-15 16:27:13 -0700

    Created custom cursor
    On JS, I made it so that when the user clicks on the big paint brush, the cursor now turns into a paint brush. Will now work on the canvas.

---
+++ etch-a-sketch.js
@@ -0,0 +1,32 @@
+const paint_brush = document.querySelector(".paint_brush");
+const canvas = document.querySelector(".canvas");
+const slider = document.querySelector(".slider");
+
+paint_brush.addEventListener("click", function(){
+    canvas.setAttribute("style", "cursor: url('./Images/cursor_brush.png'), auto;");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
